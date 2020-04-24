d8eb13449fd784c693138ad4750b6bbd8d024607
Nick Parks <nickparksdev@gmail.com>
2020-04-23 18:01:51 -0700

    Handle config properly and clean up code
    Handles loading config properly
    Handles errors much better
    Cleans up code and organizes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const CURRENT_VERSION_TAG = "1.0";
 
+//Setup our imports and variables
 const { ShortCodeExpireError, OAuthClient } = require('@mixer/shortcode-oauth');
 const rp = require('request-promise');
 const fs = require('fs');
@@ -19,13 +20,10 @@ const oAuthClient = new OAuthClient({
 
 var token;
 var refreshToken;
+var currentChannelId;
 
 //Config values
-var config = {
-    cycle_time: 60 //Default to 60 until read into config
-};
-
-var currentChannelId;
+var config = {};
 
 //Our main function
 async function start() {
@@ -38,6 +36,10 @@ async function start() {
 
     currentChannelId = getCurrentUser.channel.id;
 
+    //Load config
+    let data = JSON.parse(fs.readFileSync("./data/config.json").toString());
+    config.cycle_time = data.cycle_time;
+
     setInterval(() => {
         runAd();
     }, 1000 * 60 * config.cycle_time); //Cycle time in minutes
@@ -45,12 +47,11 @@ async function start() {
     setInterval(() => {
         getNewTokensFromRefresh(false);
     }, 1000 * 60 * 60 * 5); //Expiry time for token
-
-    runAd(); //Run ad on start for testing
 }
 
 //Sends the post request to play an ad
 function runAd() {
+    console.log("Running an AD at " + new Date().toTimeString());
     rp.post(`https://mixer.com/api/v2/ads/channels/${currentChannelId}`, {
         headers: {
             'User-Agent': 'MixBreak',
@@ -61,7 +62,7 @@ function runAd() {
         },
         json: true
     }).then((result) => {
-        console.log("Result: ", result);
+        //Default to nothing for response - Mixer only returns the channel ID anyways so
     }).catch((err) => {
         let errorCode = err.error.errorCode;
         let errorMessage = err.error.errorMessage;
@@ -69,7 +70,45 @@ function runAd() {
     });
 }
 
-//Get OAuth
+//Ran when no config is found
+function runFirstStart() {
+    console.log("Welcome to MixBreak! Automagically running ADs for you on Mixer!");
+    console.log("Please answer the following for your configuration: ");
+
+    rl.question("How often do you want to run an ad (in minutes)?", function (answer) {
+        if (isNaN(answer)) {
+            console.log("You entered an invalid number, so we're defaulting it to 1 hour!");
+            config.cycle_time = 60;
+        } else {
+            if (answer < 15) {
+                config.cycle_time = 15;
+                console.log("Mixer only allows one ad every 15 minutes. Defaulting to 15.");
+            } else {
+                config.cycle_time = answer;
+            }
+        }
+
+        fs.writeFileSync('./data/config.json', JSON.stringify(config));
+
+        rl.close();
+    });
+
+    rl.on("close", function () {
+        //If someone closes the program while mid config setup
+        if (config.cycle_time == undefined) {
+            process.exit(0);
+        }
+
+        console.log("Booting up MixBreak now!");
+
+        //Get auth now that we've setup the config
+        startAttempts();
+    });
+}
+
+///////////////////////
+//    OAUTH Helpers  //
+///////////////////////
 function startAttempts() {
     attempt().then(tokens => {
         token = tokens.data.accessToken;
@@ -132,6 +171,7 @@ function getNewTokensFromRefresh(startAfter) {
                 console.error("Failed to write new tokens to file..");
             }
 
+            //If we are refreshing on load we can start
             if (startAfter) {
                 start();
             }
@@ -141,34 +181,22 @@ function getNewTokensFromRefresh(startAfter) {
     });
 }
 
-//Ran when no config is found
-function runFirstStart() {
-    console.log("Welcome to MixBreak! Automagically running ADs for you on Mixer!");
-    console.log("Please answer the following for your configuration: ");
+//Version checker
+rp('https://api.github.com/repos/NickParks/MixBreak/releases/latest', {
+    headers: {
+        'User-Agent': "MixBreak"
+    },
+    json: true
+}).then((value) => {
+    if (value.tag_name != CURRENT_VERSION_TAG) {
+        console.log('\x1b[36m%s\x1b[0m', "There is a new version available for download!");
+        console.log('\x1b[36m%s\x1b[0m', value.url);
+    }
+}).catch(err => {
+    //Error getting github
+    console.log("Failed to check for updates");
+});
 
-    rl.question("How often do you want to run an ad (in minutes)?", function (answer) {
-        if (isNaN(answer)) {
-            console.log("You entered an invalid number, so we're defaulting it to 1 hour!");
-            config.cycle_time = 60;
-        } else {
-            if (answer < 15) {
-                config.cycle_time = 15;
-                console.log("Mixer only allows one ad every 15 minutes. Defaulting to 15.");
-            }
-        }
-
-        fs.writeFileSync('./data/config.json', JSON.stringify(config));
-
-        rl.close();
-    });
-
-    rl.on("close", function () {
-        console.log("Booting up MixBreak now!");
-
-        //Get auth now that we've setup the config
-        startAttempts();
-    });
-}
 
 //Startup script
 if (!fs.existsSync('./data')) {
@@ -207,27 +235,9 @@ if (!fs.existsSync('./data/config.json')) {
 
                 //Get new ones
                 getNewTokensFromRefresh(true);
-
-                return;
             } else {
                 startAttempts();
             }
         });
     }
 }
-
-//Version checker - Disabled while testing
-// rp('https://api.github.com/repos/NickParks/MixBreak/releases/latest', {
-//     headers: {
-//         'User-Agent': "MixBreak"
-//     },
-//     json: true
-// }).then((value) => {
-//     if (value.tag_name != CURRENT_VERSION_TAG) {
-//         console.log('\x1b[36m%s\x1b[0m', "There is a new version available for download!");
-//         console.log('\x1b[36m%s\x1b[0m', value.url);
-//     }
-// }).catch(err => {
-//     //Error getting github
-//     console.log("Failed to check for updates");
-// });
